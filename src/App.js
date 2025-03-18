498ac9e588a959ab7873cebbf33cdca87056d2f2
Chandu <chanduvalluri22@gmail.com>
2025-03-18 18:30:18 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 import Headerr from './components/Headerr';
 import Homecontainer from './components/Homecontainer';
 import Cardsection from './components/Cardsection';
-import Trackcalories from './components/Coaches';
 import Coaches from './components/Coaches';
 import ProfilePage from './components/pages/ProfilePage';
 
-import {Routes, Route} from 'react-router-dom';
-
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-     <Headerr/>
-     <Routes>
-      <Route path='/Profile'element={<ProfilePage/>}/>
-     </Routes>
-      <Homecontainer/>
-      <Cardsection/>
-      <Coaches/>
-       
-      
-      
+      {/* Only show Header, Homecontainer, Cardsection, and Coaches when not on '/Profile' */}
+      <Routes>
+        <Route
+          path="/Profile"
+          element={
+            <>
+            <Headerr/>
+          <ProfilePage />
+            </>
+          } // Only render ProfilePage without other layout components
+        />
+        <Route
+          path="/"
+          element={
+            <>
+              <Headerr />
+              <Homecontainer />
+              <Cardsection />
+              <Coaches />
+            </>
+          }
+        />
+      </Routes>
     </div>
   );
 }
--- src/components/pages/ProfilePage.js
+++ src/components/pages/ProfilePage.js
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from 'react';
+import Headerr from '../Headerr';
 
 const ProfilePage = () => {
+
+    
+
   return (
-    <div>
-        Profile example
+    
+    <div className='grid-container'>
+      <div className='griditem item-1'>
+        <div className='headersec'>
+          <p>
+          Log Food
+          </p>
+          </div>
+        <div className='searchdiv'>
+          <input type='search' className='searchbar' placeholder='Search for Food'/>
+          <button className='searchbutton'>
+            <img src='https://static-00.iconduck.com/assets.00/magnifying-glass-icon-1022x1024-r7tyhsec.png' className='searchicoon'/>
+          </button>
         </div>
+        <div className='itemcontainer'>
+            <div className='items'>
+              <p>Oatmeal</p>
+              <p>300</p>
+            </div>
+            <div className='itemdesc'>
+              <h6>P:{"20g"}</h6>
+              <h6>F:{"15g"}</h6>
+              <h6>C:{"200g"}</h6>
+              <button>+</button>
+              </div>
+          </div>
+          <div className='itemcontainer'>
+            <div className='items'>
+              <p>Oatmeal</p>
+              <p>300</p>
+            </div>
+            <div className='itemdesc'>
+              <h6>P:{"20g"}</h6>
+              <h6>F:{"15g"}</h6>
+              <h6>C:{"200g"}</h6>
+              <button>+</button>
+              </div>
+          </div>
+          <div className='itemcontainer'>
+            <div className='items'>
+              <p>Oatmeal</p>
+              <p>300</p>
+            </div>
+            <div className='itemdesc'>
+              <h6>P:{"20g"}</h6>
+              <h6>F:{"15g"}</h6>
+              <h6>C:{"200g"}</h6>
+              <button>+</button>
+              </div>
+          </div>
+          
+
+      </div>
+      <div className='griditem item-2'>Item 2</div>
+    </div>
+
   )
 }
 
-export default ProfilePage;
+export default ProfilePage
