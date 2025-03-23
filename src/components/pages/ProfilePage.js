8f5fbdde5a8b114fe3185b7b759098035e1801e5
Chandu <chanduvalluri22@gmail.com>
2025-03-23 21:47:08 +0530

    six commit

--- src/components/pages/ProfilePage.js
+++ src/components/pages/ProfilePage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Headerr from '../Headerr';
 import Popupwindow from './Popupwindow';
 import { IoMdClose } from "react-icons/io";
+import { data } from 'react-router-dom';
 
 
 
@@ -9,6 +10,8 @@ import { IoMdClose } from "react-icons/io";
 const ProfilePage = () => {
 
   const [datares, setdatares] = useState([]);
+  const [filtereddata, setfiltereddata] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   const varw = async () => {
     try {
@@ -16,17 +19,51 @@ const ProfilePage = () => {
       const resjson = await res.json();
       console.log(resjson.fooditems);
       setdatares(resjson);
+      setfiltereddata(resjson.fooditems);
     } catch (error) {
       console.log('error reading the data');
     }
 
-
+    
 
   }
 
+  
+
   useEffect(() => {
     varw();
+    
+    
   }, []);
+
+  
+
+  const searchval = () => {
+    const searched = document.getElementById('searchvall').value;
+    if (searched) {
+      const filterdata = filtereddata.filter(item =>  item.name === searched);
+      console.log(filterdata);
+      setfiltereddata(filterdata);
+    } else {
+      console.log('no value')
+    }
+    
+    
+    
+  }
+    
+  
+
+  
+    
+  
+
+
+  
+
+
+  
+  
 
 
 
@@ -42,23 +79,29 @@ const ProfilePage = () => {
           </p>
         </div>
         <div className='searchdiv'>
-          <input type='search' className='searchbar' placeholder='Search for Food' />
-          <button className='searchbutton'>
+          <input type='search'className='searchbar' id='searchvall' placeholder='Search for Food' />
+          <button className='searchbutton' onClick={searchval}>
             <img src='https://static-00.iconduck.com/assets.00/magnifying-glass-icon-1022x1024-r7tyhsec.png' className='searchicoon' />
           </button>
         </div>
-        <div className="itemcontainer">
-          <div className="items">
-            <p>Oatmeal</p>
-            <p>300Cal</p>
+        {filtereddata.length > 0 ? (
+          filtereddata.map((item, index) => (
+            <div key={index} className="itemcontainer">
+            <div className="items">
+              <p>{item.name}</p>
+              <p>300Cal</p>
+            </div>
+            <div className="itemdesc">
+              <h6>P: 20g</h6>
+              <h6>F: 15g</h6>
+              <h6>C: 200g</h6>
+              <button onClick={() => { setopenpopup(true) }}>+</button>
+            </div>
           </div>
-          <div className="itemdesc">
-            <h6>P: 20g</h6>
-            <h6>F: 15g</h6>
-            <h6>C: 200g</h6>
-            <button onClick={() => { setopenpopup(true) }}>+</button>
-          </div>
-        </div>
+          ))
+          
+        ): <p></p>}
+       
         {
           openpopup && (
             <div className='popup'>
